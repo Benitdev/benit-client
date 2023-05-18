554b6595b05b867031991d27020c51609d446851
benitdev <benitdev@gmail.com>
2023-05-18 08:45:21 +0700

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable} ${openSans.variable}`}>
-      <body className="bg-slate-900 text-base">
+      <body className="bg-slate-900 text-base ">
         <div id="root">{children}</div>
       </body>
     </html>
